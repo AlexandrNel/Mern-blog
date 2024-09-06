05f2c82b87e0f7c0a25eda3bc6a52fb1b0adf685
Alex <sashanelyubov2005@gmail.com>
2024-09-07 00:48:50 +0300

    refactoring

---
+++ backend/controllers/index.js
@@ -0,0 +1,5 @@
+import { registerController } from "./register.js";
+import { loginController } from "./login.js";
+import { getMe } from "./getMe.js";
+
+export { registerController, loginController, getMe };
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,16 +1,12 @@
 import express from "express";
-import { validationResult } from "express-validator";
-import * as bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import { registerValidation } from "../utils/registerValidation.js";
-import UserModel from "../models/user.js";
-import mongoose from "mongoose";
 import { checkAuth } from "../middlewares/checkAuth.js";
-import { registerController } from "../controllers/register.js";
-import { loginController } from "../controllers/login.js";
-import { getMe } from "../controllers/getMe.js";
+import { registerValidation } from "../utils/registerValidation.js";
+import {
+  getMe,
+  registerController,
+  loginController,
+} from "../controllers/index.js";
 
-const SECRET_KEY = "secret12345";
 export const authRouter = express.Router();
 
 authRouter.post("/auth/register", registerValidation, registerController);
