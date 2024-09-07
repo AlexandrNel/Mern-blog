cd54be2e4902cb66a10edaec40dd52aa997391c4
Alex <sashanelyubov2005@gmail.com>
2024-09-07 22:30:15 +0300

    refactoring

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,11 @@
 import express from "express";
 import { checkAuth } from "../middlewares/checkAuth.js";
-import { registerValidation } from "../utils/registerValidation.js";
+import { registerValidation } from "../utils/validations.js";
 import { authController } from "../controllers/index.js";
 const router = express.Router();
 
 router.post("/auth/register", registerValidation, authController.register);
-
 router.post("/auth/login", authController.login);
-
 router.post("/auth/me", checkAuth, authController.getMe);
 
 export default router;
