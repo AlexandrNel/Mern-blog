fa32a2cc0f945ab61515d552281f54b3cf8e4157
Alex <sashanelyubov2005@gmail.com>
2024-09-10 00:49:49 +0300

    dev

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,28 @@
 import React from "react";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import PostPage from "./pages/PostPage";
+import AddPost from "./pages/AddPost";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-  const [count, setCount] = React.useState(0);
-
-  return <>Hello</>;
+  return (
+    <>
+      <Header />
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/posts/:id" element={<PostPage />}></Route>
+          <Route path="/add-post" element={<AddPost />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="*" element={<>not found</>}></Route>
+        </Routes>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@/components/ui/theme-provider.tsx";
 import App from "./App.tsx";
 import "./global.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>
 );
