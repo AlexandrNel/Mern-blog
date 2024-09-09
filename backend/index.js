cfc8e7881e16915d68283f9bd0dc70f6abbc9134
Alex <sashanelyubov2005@gmail.com>
2024-09-09 14:37:46 +0300

    added uploading images

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 import express from "express";
+
 import authRouter from "./routes/auth.js";
 import postRouter from "./routes/posts.js";
+import uploadRouter from "./routes/upload.js";
 import { connectToDatebase } from "./database/connect.js";
 
 const PORT = 3000;
 const app = express();
+
 connectToDatebase();
 
-app.use(express.json(), authRouter, postRouter);
+app.use(express.json(), uploadRouter, authRouter, postRouter);
 
 app.listen(PORT, (err) => (err ? console.log(err) : console.log("Server OK")));
