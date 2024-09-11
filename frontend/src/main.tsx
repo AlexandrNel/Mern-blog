175820049aa26d2b43937d1cc720e33194386563
Alex <sashanelyubov2005@gmail.com>
2024-09-11 20:46:48 +0300

    adding redux

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
 import { ThemeProvider } from "@/components/ui/theme-provider.tsx";
 import App from "./App.tsx";
 import "./global.css";
@@ -9,7 +11,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
