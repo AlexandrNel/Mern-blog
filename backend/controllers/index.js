786543198dbdebcd72b0c77dfd9000a80ecdcaae
Alex <sashanelyubov2005@gmail.com>
2024-10-03 21:43:55 +0300

    refactor: move comment logic to a separate controller

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,4 +1,5 @@
 import * as authController from "./authController.js";
 import * as PostController from "./postController.js";
 import * as LikeContoller from "./likeController.js";
-export { authController, PostController, LikeContoller };
+import * as CommentController from "./commentController.js";
+export { authController, PostController, LikeContoller, CommentController };
